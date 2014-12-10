2d4a268c77ab2399660bef822511361db7ec5f0d
huangxianshuai <1071240901@qq.com>
2014-12-10 10:31:39 +0800

    Initial Commit

---
+++ Strikethrough/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Strikethrough
+//
+//  Created by Huang Xianshuai on 14/12/10.
+//  Copyright (c) 2014年 Huang Xianshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
