7212684a6aa3e3a9627c58ec952711c23e700b39
HXsCoding <huangxianshuai@syvip.com>
2014-12-12 16:03:31 +0800

    first commit

--- Strikethrough/ViewController.m
+++ Strikethrough/ViewController.m
@@ -10,14 +10,61 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
 @end
 
 @implementation ViewController
 
+- (IBAction)setLineSpacing:(id)sender {
+    
+    NSString *content = @"￥8888888888888888888888888888888888888888888888";
+    
+    NSMutableAttributedString *attributeString = [[NSMutableAttributedString alloc] initWithString:content];
+    
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.lineSpacing = 10.0f;
+    
+    [attributeString addAttribute:NSParagraphStyleAttributeName value:paragraphStyle range:NSMakeRange(0, content.length)];
+    _label.attributedText = attributeString;
+}
+
+
+- (IBAction)showStrikethrough:(id)sender {
+    
+    NSString *content = @"7777777777777777777777777777777777777";
+    NSMutableAttributedString *attributeStr = [[NSMutableAttributedString alloc] initWithString:content];
+    [attributeStr addAttributes:@{ NSStrikethroughStyleAttributeName : @(1), NSFontAttributeName : [UIFont fontWithName:@"HelveticaNeue-Light" size:16.0f] } range:NSMakeRange(0, content.length)];
+    _label.attributedText = attributeStr;
+    
+    
+    // NSStrikethroughStyleAttributeName  删除线的粗度
+    // NSFontAttributeName 字体设置 （可有可无）
+
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
 }
+
+/*
+- (void)setChatContent:(NSString *)theContent {
+    
+    if (![chatContent isEqualToString:theContent]) {
+        chatContent = theContent;
+        //        _chatContentLabel.text = chatContent;
+        NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:chatContent];
+        NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+        [paragraphStyle setLineSpacing:5];
+        [attributedString addAttribute:NSParagraphStyleAttributeName
+                                 value:paragraphStyle
+                                 range:NSMakeRange(0, [chatContent length])];
+        _chatContentLabel.attributedText = attributedString ;
+    }
+}
+ */
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
